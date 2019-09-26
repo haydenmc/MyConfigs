4ed93f1efdba51959c59b4f9e3bd6917b4f7e5b8
Hayden McAfee <hamc@microsoft.com>
2019-09-26 15:15:53 -0700

    Update msedge to not use absolute path

--- bin/groupme.bat
+++ bin/groupme.bat
@@ -1,1 +1,1 @@
-cd "%PROGRAMFILES(X86)%\Microsoft\Edge Dev\Application" && start msedge --profile-directory=Default --app=https://web.groupme.com
+start msedge --profile-directory=Default --app=https://web.groupme.com
