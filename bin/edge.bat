5fb635edd5291b11fec4a7e3ac34fa0e071e106e
Hayden McAfee <hayden@outlook.com>
2020-10-15 11:05:07 -0700

    Work profile nonsense

--- bin/edge.bat
+++ bin/edge.bat
@@ -1,1 +1,1 @@
-start "" "microsoft-edge:%*"
+start msedge --profile-directory="Default"
--- bin/owa.bat
+++ bin/owa.bat
@@ -1,1 +1,1 @@
-start msedge --profile-directory=Default --app=https://outlook.office.com/owa/?realm=microsoft.com
+start msedge --profile-directory="Profile 1" --app=https://outlook.office.com/owa/?realm=microsoft.com
